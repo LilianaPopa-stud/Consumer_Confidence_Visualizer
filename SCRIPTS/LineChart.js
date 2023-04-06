e806f1fa00209062355763f21d88649323e9bee4
aanaolaru <79383164+aanaolaru@users.noreply.github.com>
2023-04-06 13:56:35 -0700

    linech + table

---
+++ SCRIPTS/LineChart.js
@@ -0,0 +1,78 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Month', 'Italy', 'Germany', 'France', 'Spain'],
+    ['Apr-2020', 100.5, 95, 98.4, 97],
+    ['May-2020', 99, 103, 97, 101.6],
+    ['Jun-2020', 98.4, 96, 93, 101.4],
+    ['Apr-2021', 100.5, 95.3, 97.8, 100],
+    ['May-2021', 97, 96, 94, 102],
+    ['Jun-2021', 98.1, 95, 95.9, 102],
+    ['Apr-2022', 98.5, 96.8, 96.5, 101],
+    ['May-2022', 95, 98, 97, 100],
+    ['Jun-2022', 96.5, 96.5, 96.5, 99.5]
+  ]);
+
+  var options = {
+    title: 'CCI between 2018 - 2020',
+    titleTextStyle: {
+        color: 'black',
+        fontName: 'Oxygen',
+        fontSize: '25',},
+
+        annotations: {
+            textStyle: {
+                color: 'black'
+            },
+            fontName: 'Oxygen',
+            fontSize: '25',
+        },
+        hAxis: {
+            title: 'Month',
+            textStyle: {
+                color: 'black'
+            },
+            titleTextStyle: {
+                color: 'black',
+            }
+        },
+        vAxis: {
+            title: 'Index',
+            textStyle: {
+                color: 'black'
+            },
+            titleTextStyle: {
+                color: 'black',
+            }
+        },
+        seriesType: 'line',
+        series: {
+            0: {
+
+                color: "#a80874"
+            },
+            1: {
+                color: "#C490D1"
+            },
+            2: {
+                color: "#631A86"
+            },
+            3: {
+                color: "#a3c3d9"
+            },
+           
+        },
+        color: "#C490D1",
+        backgroundColor: 'transparent',
+        legendTextStyle: {color: 'black', fontSize: 12, fontName: 'Oxygen'},
+        width: 1000,
+        height: 500,
+        curveType: 'function',
+        legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('linechart-div'));
+  chart.draw(data, options);
+}
---
+++ SCRIPTS/Table.js
@@ -0,0 +1,49 @@
+google.charts.load('current', {'packages':['table']});
+google.charts.setOnLoadCallback(drawTable);
+
+function drawTable() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Location');
+    data.addColumn('number', 'Apr-2020');
+    data.addColumn('number', 'May-2020');
+    data.addColumn('number', 'Jun-2020');
+    data.addColumn('number', 'Apr-2021');
+    data.addColumn('number', 'May-2021');
+    data.addColumn('number', 'Jun-2021');
+    data.addColumn('number', 'Apr-2022');
+    data.addColumn('number', 'May-2022');
+    data.addColumn('number', 'Jun-2022');
+
+    data.addRows([
+        ['Italy',	100.5, 99, 98.4,
+        100.5, 97, 98.1,
+        98.5, 95, 96.5,],
+        ['Germany',	95, 103, 96, 
+        95.3, 96, 95,
+        96.8, 98, 96.5],
+        ['France',	98.4, 97, 93,
+        97.8, 94, 95.9,
+        96.5, 97, 96.5],
+        ['Spain',97, 101.6, 101.4, 
+        100, 102, 102,
+        101, 100, 99.5],
+    ]);
+    var cssClassNames = {
+        'headerRow': 'cssHeaderRow',
+        'tableRow': 'cssTableRow',
+        'oddTableRow': 'cssOddTableRow',
+        'selectedTableRow': 'cssSelectedTableRow',
+        'hoverTableRow': 'cssHoverTableRow',
+        'headerCell': 'cssHeaderCell',
+        'tableCell': 'cssTableCell',
+    };
+
+    var options = {
+        showRowNumber: false,
+        cssClassNames: cssClassNames
+
+    };
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+
+    table.draw(data, options);
+}
