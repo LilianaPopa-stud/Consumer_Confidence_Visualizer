174ed38a67baaa090db1976d3354b313563a88f6
LilianaPopa-stud <popliliaana@gmail.com>
2023-06-15 18:17:56 +0300

    setup server

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 6789;
 const url = require('url');
 const fs = require('fs');
-const server = http.createServer(async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+const server = http.createServer( (req, res) => {
+
     res.statusCode = 200;
     const reqUrl = url.parse(req.url);
     const path = reqUrl.pathname;
     if(path === '/'){
         directHTML('./HTML/INDEX.html', res);
+    }
+    else if(path === './CSS/style.css'){
+        fs.readFile('./src/webapp/public/styles/loader.css', function (err, page) {
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(page);
+            res.end();
+        });
     }
 });
 function directHTML(path, res){
