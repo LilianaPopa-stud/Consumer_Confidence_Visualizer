6be5a5c4125b931a7941a940396e7a21013c0617
olaruandreea12 <39705342+olaruandreea12@users.noreply.github.com>
2023-06-15 22:23:08 +0300

    Update connectionDatabase.js

---
+++ server/connectionDatabase.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require('sequelize');
+/**
+ * Initializing Sequelize ORM
+ * */
+const db = new Sequelize("consumer_confidence",null,null,{
+    dialect : "sqlite",
+    storage: "../consumer_confidence.db"
+});
+
+module.exports = db;
