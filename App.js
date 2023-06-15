e7c9538123093abc9aa14ae48eaa3ddc3792ac4f
olaruandreea12 <39705342+olaruandreea12@users.noreply.github.com>
2023-06-15 18:15:38 +0300

    app.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const url = require('url');
 const fs = require('fs');
+
 const server = http.createServer(async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.statusCode = 200;
@@ -10,7 +11,44 @@ const server = http.createServer(async (req, res) => {
     const path = reqUrl.pathname;
     if(path === '/'){
         directHTML('./HTML/INDEX.html', res);
+    } else if (path === '/Report.html'){
+        directHTML('./HTML/Report.html', res)
+    } else if (path === '/Charts.html') {
+        directHTML('./HTML/Charts.html', res)
+    } else if (path === '/Contact.html') {
+        directHTML('./HTML/Contact.html', res)
+    } else if (path === '/AdminLogIn.html') {
+        directHTML('./HTML/AdminLogIn.html', res)
+    } else if (path === '/AdminPanel.html') {
+        directHTML('./HTML/AdminPanel.html', res)
+    } else if(path === '/admin.css'){
+       directCSS('./CSS/admin.css')
+    } else if(path === 'adminPanel.css'){
+        directCSS('./CSS/adminPanel.css')
+    }  else if(path === 'charts.css'){
+        directCSS('./CSS/charts.css')
+    }  else if(path === 'map.css'){
+        directCSS('./CSS/map.css')
+    } else if(path === 'report.css'){
+        directCSS('./CSS/report.css')
+    }  else if(path === 'style.css'){
+        directCSS('./CSS/style.css')
+    }  else if(path === 'table.css'){
+        directCSS('./CSS/table.css')
+    } else if(path == 'BarChart.js') {
+        directJS('.SCRIPTS/BarChart.js')
+    } else if(path == 'ChartSelect.js') {
+        directJS('.SCRIPTS/ChartSelect.js')
+    } else if(path == 'CoreChart.js') {
+        directJS('.SCRIPTS/CoreChart.js')
+    } else if(path == 'GeoChart.js') {
+        directJS('.SCRIPTS/GeoChart.js')
+    } else if(path == 'LineChart.js') {
+        directJS('.SCRIPTS/LineChart.js')
+    } else if(path == 'Table.js') {
+        directJS('.SCRIPTS/Table.js')
     }
+
 });
 function directHTML(path, res){
     fs.readFile(path, function (err, data) {
@@ -18,6 +56,32 @@ function directHTML(path, res){
             res.statusCode = 404;
             res.end('Not found');
         }else{
+            res.write(data);
+            res.end();
+        }
+    });
+}
+
+function directCSS(path, res) {
+    fs.readFile(path, function (err, data) {
+        if(err){
+            res.statusCode = 404;
+            res.end('Not found');
+        } else{
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(data);
+            res.end();
+        }
+    });
+}
+
+function directJS(path, res) {
+    fs.readFile(path, function (err, data)) {
+        if(err){
+            res.statusCode = 404;
+            res.end('Not found');
+        } else{
+            res.writeHead(200, {'Content-Type': 'application/javascript'});
             res.write(data);
             res.end();
         }
