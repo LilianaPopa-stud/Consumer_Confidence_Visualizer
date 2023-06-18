58f7d1c216bbaf2623558ee08becc4c72eaeb212
aanaolaru <79383164+aanaolaru@users.noreply.github.com>
2023-06-18 10:45:02 -0700

    exportCSV.js

---
+++ SCRIPTS/exportCSV.js
@@ -0,0 +1,52 @@
+
+function generateCSVTable(table) {
+    let csvTable = ""
+    const tableString = table.innerText 
+    const tableString_array = tableString.split("\n") 
+    if (table == document.getElementById("myChart1")){ // pt tabel
+        for (let i = 0;i<tableString_array.length;i++)
+        {
+            const row_split = tableString_array[i].split("\t")
+                for(let index_element = 0; index_element<row_split.length;index_element++)
+                {
+                    csvTable += `${row_split[index_element]},`
+                }
+                csvTable = csvTable.slice(0,-1)
+    
+            csvTable += '\n'
+        }
+        return csvTable;
+    }
+    else{ // pt restul 
+        for (let i = 1;i<tableString_array.length;i++)
+        {
+            const row_split = tableString_array[i].split("\t")
+                for(let index_element = 0; index_element<row_split.length;index_element++)
+                {
+                    csvTable += `${row_split[index_element]},`
+                }
+                csvTable = csvTable.slice(0,-1)
+    
+            csvTable += '\n'
+        }
+        return csvTable;
+    }
+
+}
+
+function downloadCSV(filename, csvStringContent) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvStringContent));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+
+function get_export_csv(classId) {
+    let table = document.getElementById(classId)
+    downloadCSV("export.csv",generateCSVTable(table))
+}
+
